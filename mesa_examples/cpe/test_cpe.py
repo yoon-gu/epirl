35fc1f43aad6a9fbedf5a40e954c4834a217db86
Yoon-gu Hwang <yz0624@gmail.com>
2022-08-11 22:43:27 +0900

    cpe test 코드 수정

--- mesa_examples/cpe/test_cpe.py
+++ mesa_examples/cpe/test_cpe.py
@@ -1,15 +1,27 @@
 import pytest
 from CPEmodel import CPE_Model
-from CPEmodel import height, width
 
 def test_cpe_model():
-    numPatients = 30
-    numHCW = 4
-    numGoo = 3
-    model = CPE_Model(num_HCWs=numHCW, num_Patients=numPatients,
-                      num_Goo=numGoo, prob_patient_sick=0.01,
-                      prob_new_patient=0.05, cleaningDay=40,
-                      prob_transmission=.1, isolation_factor=.5,
-                      isolate_sick=True, icu_hcw_wash_rate=.9,
-                      outside_hcw_wash_rate=.9,
-                      height=height, width=width)
+    # Parameters
+    probPatientSick = 0.01
+    probNewPatient = 0.003
+    probTransmission = 0
+    isolationFactor = 0.75 # fix
+    isolationTime = 14
+    cleanDay = 360
+    isolateSick = True
+    ICUwashrate = 0.90
+    OUTSIDEwashrate = 0.90
+    height=11
+    width=32
+
+    # Specify the variable I want to change separately.
+    beta = [0.000001, 0.000002]
+    variable_params = {"prob_transmission" : beta}
+    # %% STEP4
+    model = CPE_Model(
+        prob_patient_sick=probPatientSick,prob_new_patient=probNewPatient, prob_transmission=probTransmission,
+        isolation_factor=isolationFactor,cleaningDay=cleanDay, isolate_sick=True, isolation_time=isolationTime,
+        icu_hcw_wash_rate=ICUwashrate, outside_hcw_wash_rate=OUTSIDEwashrate,
+        height=height, width=width
+        )
