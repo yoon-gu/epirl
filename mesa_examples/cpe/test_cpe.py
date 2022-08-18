fbc9ce8cb6d1ecea76ead6f0710737ce7bb8a09c
Yoon-gu Hwang <yz0624@gmail.com>
2022-08-18 16:43:23 +0900

    test 메소드 누락 수정

--- mesa_examples/cpe/test_cpe.py
+++ mesa_examples/cpe/test_cpe.py
@@ -1,25 +1,26 @@
 from CPEmodel import CPE_Model
 
-# Parameters
-probPatientSick = 0.01
-probNewPatient = 0.003
-probTransmission = 0
-isolationFactor = 0.75
-isolationTime = 14
-cleanDay = 360
-isolateSick = True
-ICUwashrate = 0.90
-OUTSIDEwashrate = 0.90
-height=11
-width=32
+def test_cpe_model():
+    # Parameters
+    probPatientSick = 0.01
+    probNewPatient = 0.003
+    probTransmission = 0
+    isolationFactor = 0.75
+    isolationTime = 14
+    cleanDay = 360
+    isolateSick = True
+    ICUwashrate = 0.90
+    OUTSIDEwashrate = 0.90
+    height=11
+    width=32
 
-model = CPE_Model(
-    prob_patient_sick=probPatientSick, prob_new_patient=probNewPatient,
-    prob_transmission=probTransmission,
-    isolation_factor=isolationFactor,
-    cleaningDay=cleanDay, isolate_sick=True,
-    isolation_time=isolationTime,
-    icu_hcw_wash_rate=ICUwashrate,
-    outside_hcw_wash_rate=OUTSIDEwashrate,
-    height=height, width=width
-    )
+    model = CPE_Model(
+        prob_patient_sick=probPatientSick, prob_new_patient=probNewPatient,
+        prob_transmission=probTransmission,
+        isolation_factor=isolationFactor,
+        cleaningDay=cleanDay, isolate_sick=True,
+        isolation_time=isolationTime,
+        icu_hcw_wash_rate=ICUwashrate,
+        outside_hcw_wash_rate=OUTSIDEwashrate,
+        height=height, width=width
+        )
