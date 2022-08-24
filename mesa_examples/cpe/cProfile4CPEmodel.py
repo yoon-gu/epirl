c3d90da67e6db0934874a4fbfee704f1f3f3d797
KimSol <kimsol729@yonsei.ac.kr>
2022-08-24 19:06:32 +0900

    Profiling Batch Run

--- mesa_examples/cpe/cProfile4CPEmodel.py
+++ mesa_examples/cpe/cProfile4CPEmodel.py
@@ -1,5 +1,10 @@
 import cProfile
 from CPEmodel import CPE_Model
+from mesa.batchrunner import BatchRunner
+from CPEmodel import getNumSick
+
+num_iter = 1
+runtime = 200
 
 probPatientSick = 0.01
 probNewPatient = 0.003
@@ -13,6 +18,23 @@ OUTSIDEwashrate = 0.90
 height=11
 width=32
 
+fixed_params = {
+    "prob_patient_sick" : probPatientSick, 
+    "prob_new_patient" : probNewPatient, 
+    # "prob_transmission" : probTransmission,
+    "isolation_factor" : isolationFactor, 
+    "cleaningDay" : cleanDay, 
+    "isolate_sick" : isolateSick,
+    "isolation_time" : isolationTime, 
+    "icu_hcw_wash_rate" : ICUwashrate, 
+    "outside_hcw_wash_rate" : OUTSIDEwashrate, 
+    "height" : height, "width" : width 
+    }
+
+# Specify the variable I want to change separately.
+beta = [0.00001, 0.00002]
+variable_params = {"prob_transmission" : beta}
+
 model = CPE_Model(
     prob_patient_sick=probPatientSick, prob_new_patient=probNewPatient,
     prob_transmission=probTransmission,
@@ -24,10 +46,14 @@ model = CPE_Model(
     height=height, width=width
     )
 
-print(cProfile.run("model.step()","profiling/cpe_cProfiling.prof"))
-# Enter the " snakeviz profiling/cpe_cProfiling.prof " in cmd window.
+batch_run = BatchRunner(
+    CPE_Model,
+    variable_parameters = variable_params,
+    fixed_parameters = fixed_params,
+    iterations=num_iter,
+    max_steps=model.ticks_in_day * runtime,
+    model_reporters = {"Number_of_Patients_sick":getNumSick}
+)
 
-# (1) checkFilled 를 매번 할 필요 없음
-# (2) get_cell_list_contents 보다 효율적인 방법
-# (3) IsolatedBed 의 step() 은 checkFilled를 두 번 포함 (1)을 해결하면 될 것 같다.
-# (4) Nurse 의 step()은 summon 보다 move and spread <-해결방법 고민.
+print(cProfile.run("batch_run.run_all()","profiling/BR_cProfiling.prof"))
+# Enter the " snakeviz profiling/BR_cProfiling.prof " in cmd window.
