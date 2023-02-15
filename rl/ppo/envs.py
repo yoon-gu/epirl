d06bbf70e8e802975ef86dc67bc753a05b2b2499
Yoon-gu Hwang <yz0624@gmail.com>
2023-02-16 00:48:33 +0900

    fix bug

--- rl/ppo/envs.py
+++ rl/ppo/envs.py
@@ -20,7 +20,9 @@ class SirEnvironment(gym.Env):
     susceptible: list
     infected: list
     actions: list
-    def __init__(self, S0, I0, beta, gamma, v_min, v_max, tf):
+    vaccine_importance: float
+    continuous_actions: bool
+    def __init__(self, S0, I0, beta, gamma, v_min, v_max, tf, vaccine_importance, continuous_actions):
         self.state = np.array([S0, I0])
         self.beta = beta
         self.gamma = gamma
@@ -29,28 +31,39 @@ class SirEnvironment(gym.Env):
         self.S0 = S0
         self.I0 = I0
         self.tf = tf
+        self.vaccine_importance = vaccine_importance
+        self.continuous_actions = continuous_actions
         self.observation_space = gym.spaces.Box(
                     low=np.array([0.0, 0.0], dtype=np.float32),
                     high=np.array([1000.0, 1000.0], dtype=np.float32),
                     dtype=np.float32)
-        self.action_space = gym.spaces.Box(
-                    low=np.array([-1.0], dtype=np.float32),
-                    high=np.array([1.0], dtype=np.float32),
-                    dtype=np.float32)
+        if self.continuous_actions:
+            self.action_space = gym.spaces.Box(
+                        low=np.array([-1.0], dtype=np.float32),
+                        high=np.array([1.0], dtype=np.float32),
+                        dtype=np.float32)
+        else:
+            self.action_space = gym.spaces.Discrete(2)
         self.time = 0.0
         self.dt = 1.0
 
     def reset(self):
+        self.time = 0.0
         self.days = [self.time]
         self.susceptible = [self.S0]
         self.infected = [self.I0]
         self.actions = []
-        self.time = 0.0
         self.state = np.array([self.S0, self.I0])
         return np.array(self.state, dtype=np.float32)
 
+    def action2vaccine(self, action):
+        return self.v_min + self.v_max * (action[0] + 1.0) / 2.0
+
     def step(self, action):
-        vaccine = self.v_min + self.v_max * (action[0] + 1.0) / 2.0
+        if self.continuous_actions:
+            vaccine = self.action2vaccine(action)
+        else:
+            vaccine = self.v_min if action == 0 else self.v_max
         self.actions.append(vaccine)
 
         sol = odeint(sir, self.state, np.linspace(0, 1, 101), args=(self.beta, self.gamma, vaccine))
@@ -59,7 +72,7 @@ class SirEnvironment(gym.Env):
         S0, I0 = self.state
         S, I = new_state
         self.state = new_state
-        reward = - I - 10*vaccine
+        reward = - I - self.vaccine_importance * vaccine
 
         self.days.append(self.time)
         self.susceptible.append(S)
